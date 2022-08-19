c8a2d4b8240a1c4691dc422e4d53c9cfbd6fe4e9
Jesse <jqcruz98@gmail.com>
2022-08-18 20:48:19 -0700

    Created working slider. Slider needs to reset grid on use though

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
-/*
-Create 16 x 16 square divs using only javascript.
-createElement.
-Append child
 
-For loop, create a container div that holds 16 divs in a row
-
-container = create container div
-for(i<16, add 1 to i)
-    
-make it so that the size of the child elments scale to fit grid
-
-*/
 let gameStart = false;
+let isMouseDown = false;
 let playerPosition = 0;
 currentColor = "black"
 // # of columns * size = 400px
 let numberOfColumns = 50;
 let columnSize = "8px"
+//return # of pixels based on board size
+const boardSizeDict = {
+    7: 8, 6: 10, 5: 20, 4: 40, 3: 80, 2: 100, 1: 400
+}
 
 function sixteenColumns(height){
     let newString = "";
@@ -27,13 +20,17 @@ function sixteenColumns(height){
 }
 
 
-const daddyFlexContainer = document.querySelector("#daddyFlexContainer");
+const FlexContainer = document.querySelector("#FlexContainer");
+const colorGrid = document.querySelector("#colorGrid");
 const container = document.createElement('div');
-container.setAttribute(
-    "style", `display: grid; background-color: blue; grid-template-columns: repeat(${numberOfColumns}, ${columnSize}); grid-template-rows: repeat(${numberOfColumns}, ${columnSize}); width: fit-content; margin: 0 auto;`,
-    )
-container.setAttribute("class", "gridContainer")
-daddyFlexContainer.appendChild(container);
+
+const createGridContainer = function(){
+    container.setAttribute(
+        "style", `display: grid; grid-template-columns: repeat(${numberOfColumns}, ${columnSize}); grid-template-rows: repeat(${numberOfColumns}, ${columnSize}); width: fit-content; margin: 0 auto;`,
+        )
+    container.setAttribute("class", "gridContainer")
+    FlexContainer.insertBefore(container, colorGrid);
+    }
 // creating 16 * 16 child elements
 
 
@@ -41,46 +38,15 @@ function createChildren(container){
     for(let i = 1; i <= (numberOfColumns * numberOfColumns); i++){
         let gridChild = document.createElement("div");
         gridChild.setAttribute("class", `box b${i}`);
-        gridChild.setAttribute("style", "background-color: grey; text-align: center; padding: 10px 0px;");
         //create an attribute value so that we don't have to use innerText.
         gridChild.setAttribute("value", `${i}`);
         container.appendChild(gridChild);
     }
 }
-//change gamestart to true
-//select a starting position for the player
-
 const theClick = function (e){
-    playerPosition = parseInt(e.currentTarget.getAttribute("value"), 10);
-    const smallBoxes = document.querySelectorAll(".box");
-    for(let box of smallBoxes){
-        box.setAttribute("style", "background-color: grey;")
-    }
-    if(playerPosition == 69){
-        const gridContainer = document.querySelector(".gridContainer");
-        alert("The lord will save your soul");
-        gridContainer.style.backgroundImage = "url('./images/1.jpg')";
-        currentColor = "transparent"
-    }
-
-    e.currentTarget.setAttribute(`style`, `background-color: ${currentColor};`);
-    gameStart = true;
-}
-
-//change box class to remove hover 
-const changeBoxClass = function (){
-    const boxes = document.querySelectorAll(".box");
-    for(let box of boxes){
-        box.setAttribute("class", `boxClicked b${parseInt(box.getAttribute("value"), 10)}`);
-    }
-}
-
-const checkGameStart = function(){
-    if(gameStart === true){
-        children = document.querySelectorAll(".box");
-        for(let child of children){
-            child.removeEventListener("click", theClick);
-        }
+    if(isMouseDown === true){
+        playerPosition = parseInt(e.currentTarget.getAttribute("value"), 10);
+        e.currentTarget.setAttribute(`style`, `background-color: ${currentColor};`);
     }
 }
 
@@ -116,21 +82,56 @@ const playerMoveRight = function (e){
         }
 }
 
+const whenMouseDown = function (){
+    isMouseDown = true;
+    return isMouseDown;
+}
+
+const whenMouseUp = function(){
+    isMouseDown = false;
+    return isMouseDown;
+}
+
+const slider = document.querySelector("#myRange");
+const sliderText = document.querySelector("#sizeText");
+
+const changeBoardSize = function(){
+    let sliderValue = parseInt(slider.value);
+    columnSize = `${boardSizeDict[sliderValue]}px`;
+    numberOfColumns = 400/boardSizeDict[sliderValue];
+}
+
+const showBoardSize = function (){
+    sliderText.textContent = slider.value;
+}
+
+slider.addEventListener("input", showBoardSize);
+slider.addEventListener("input", () => container.remove())
+slider.addEventListener("input", changeBoardSize);
+slider.addEventListener("input", createGridContainer)
+slider.addEventListener("input", createChildren(container));
+slider.addEventListener("input", addClickEvents);
+
 
 //add click event listeners to all .box
 function addClickEvents(){
     children = document.querySelectorAll(".box");
     for(let child of children){
-        child.addEventListener("click", theClick);
-        child.addEventListener("click", checkGameStart);
-        child.addEventListener("click", changeBoxClass);
+        child.addEventListener("mouseover", theClick);
+        child.addEventListener("mousedown", whenMouseDown);
+        child.addEventListener("mouseup", whenMouseUp);
     }
     window.addEventListener("keydown", playerMoveDown);
     window.addEventListener("keydown", playerMoveLeft);
     window.addEventListener("keydown", playerMoveRight);
     window.addEventListener("keydown", playerMoveUp);
+    window.addEventListener("mousedown", whenMouseDown);
+    window.addEventListener("mouseup", whenMouseUp);
 }
 
+changeBoardSize();
+showBoardSize();
+createGridContainer();
 createChildren(container);
 addClickEvents();
 
