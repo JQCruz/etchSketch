326171c14b022677732fb1e27e28d5fb9e8ed0db
Jesse <jqcruz98@gmail.com>
2022-08-20 09:58:38 -0700

    Styled page, rainbow button, and slider. Basically done

--- script.js
+++ script.js
@@ -7,9 +7,17 @@ currentColor = "black"
 let numberOfColumns = 50;
 let columnSize = "8px"
 //return # of pixels based on board size
+let rainbowIndex = 1;
 const boardSizeDict = {
     7: 8, 6: 10, 5: 20, 4: 40, 3: 80, 2: 100, 1: 400
 }
+
+const rainbow = {
+    1: "red", 2: "orange", 3: "yellow", 4: "green", 5: "blue", 6: "indigo",
+    7: "violet"
+}
+
+let rainbowMode = false;
 
 function sixteenColumns(height){
     let newString = "";
@@ -19,6 +27,31 @@ function sixteenColumns(height){
     return newString;
 }
 
+const rainbowButton = document.querySelector("#rainbowMode");
+const rainbowClick = function(){
+    rainbowMode = !rainbowMode;
+    if(rainbowMode === false){
+        currentColor = "black";
+        rainbowButton.innerText = "Dark Mode";
+        rainbowButton.setAttribute("style", "background-color: #303531;")
+    }
+    else{
+        rainbowButton.innerText = "Rainbow Mode";
+        rainbowButton.setAttribute("style", "background-color: #6bd38a;")
+    }
+}
+
+const changeRainbow = function(){
+    if(rainbowMode === true){
+        currentColor = rainbow[rainbowIndex]
+        if(rainbowIndex < 7){
+            rainbowIndex++;
+        }
+        else{
+            rainbowIndex = 1;
+}}}
+
+rainbowButton.addEventListener("click", rainbowClick);
 
 const FlexContainer = document.querySelector("#FlexContainer");
 const colorGrid = document.querySelector("#colorGrid");
@@ -26,7 +59,7 @@ const container = document.createElement('div');
 
 const createGridContainer = function(){
     container.setAttribute(
-        "style", `display: grid; grid-template-columns: repeat(${numberOfColumns}, ${columnSize}); grid-template-rows: repeat(${numberOfColumns}, ${columnSize}); width: fit-content; margin: 0 auto;`,
+        "style", `display: grid; grid-template-columns: repeat(${numberOfColumns}, ${columnSize}); grid-template-rows: repeat(${numberOfColumns}, ${columnSize}); width: fit-content;`,
         )
     container.setAttribute("class", "gridContainer")
     FlexContainer.insertBefore(container, colorGrid);
@@ -40,6 +73,8 @@ function createChildren(container){
         gridChild.setAttribute("class", `box b${i}`);
         //create an attribute value so that we don't have to use innerText.
         gridChild.setAttribute("value", `${i}`);
+        gridChild.setAttribute("draggable", "false");
+        gridChild.setAttribute("style", "background-color: white;")
         container.appendChild(gridChild);
     }
 }
@@ -49,6 +84,11 @@ const theClick = function (e){
         e.currentTarget.setAttribute(`style`, `background-color: ${currentColor};`);
     }
 }
+
+const theFirstClick = function (e){
+        playerPosition = parseInt(e.currentTarget.getAttribute("value"), 10);
+        e.currentTarget.setAttribute(`style`, `background-color: ${currentColor};`);}
+
 
 const playerMoveUp = function (e){
     if(playerPosition > numberOfColumns && gameStart === true && (e.code == "KeyW" || e.code == "ArrowUp" )){
@@ -97,28 +137,39 @@ const sliderText = document.querySelector("#sizeText");
 
 const changeBoardSize = function(){
     let sliderValue = parseInt(slider.value);
+    let children = document.querySelectorAll(".box");
     columnSize = `${boardSizeDict[sliderValue]}px`;
     numberOfColumns = 400/boardSizeDict[sliderValue];
 }
 
-const showBoardSize = function (){
-    sliderText.textContent = slider.value;
+const resetBoardColor = function (){
+    let children = document.querySelectorAll(".box");
+    for(let child of children){
+        child.setAttribute("style", "background-color: white;")
+    }
 }
 
-slider.addEventListener("input", showBoardSize);
+const showBoardSize = function (){
+    sliderText.textContent = `Board-Size: ${numberOfColumns}X${numberOfColumns}`;
+}
+
 slider.addEventListener("input", () => container.remove())
 slider.addEventListener("input", changeBoardSize);
+slider.addEventListener("input", showBoardSize);
 slider.addEventListener("input", createGridContainer)
 slider.addEventListener("input", createChildren(container));
 slider.addEventListener("input", addClickEvents);
+slider.addEventListener("input", resetBoardColor);
 
 
 //add click event listeners to all .box
 function addClickEvents(){
     children = document.querySelectorAll(".box");
     for(let child of children){
-        child.addEventListener("mouseover", theClick);
         child.addEventListener("mousedown", whenMouseDown);
+        child.addEventListener("mouseover", theClick);
+        child.addEventListener("mouseover", changeRainbow);
+        child.addEventListener("mousedown", theFirstClick);
         child.addEventListener("mouseup", whenMouseUp);
     }
     window.addEventListener("keydown", playerMoveDown);
